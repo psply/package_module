4f7cdf4d16e4c0a2973b0f1bb146f30cdc35da6e
shipan <shipan@baidu.com>
2018-12-15 14:03:44 +0800

    new project for package_module

---
+++ utils/src/main/java/com/psply/mypackage/utils/network/okhttp/OKHttpManager.java
@@ -0,0 +1,18 @@
+package com.psply.mypackage.utils.network.okhttp;
+
+/**
+ * OKHttp的封装类
+ */
+public class OKHttpManager {
+
+    private static class OKHttpManagerHolder {
+        private static final OKHttpManager INSTANCE = new OKHttpManager();
+    }
+
+    public static OKHttpManager getInstance() {
+        return OKHttpManagerHolder.INSTANCE;
+    }
+
+
+
+}
